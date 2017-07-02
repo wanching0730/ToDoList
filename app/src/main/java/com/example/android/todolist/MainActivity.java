0ca2734da01698f1204dc6850d02352b3df8f349
Wan Ching <wanching_97@hotmail.com>
2017-07-02 09:36:11 +0800

    change colour of list items

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.todolist;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,17 +18,37 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etInput;
     private ListView listView;
+    private LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ll = (LinearLayout) findViewById(R.id.linear_layout);
+        ll.setBackgroundColor(getResources().getColor(R.color.colorLightBlue));
+
         etInput = (EditText) findViewById(R.id.input);
         listView = (ListView) findViewById(R.id.list_view);
 
         final ArrayList<String> doList = new ArrayList<String>();
-        final ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, doList);
+        //final ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.black_list, R.id.items, doList);
+
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, doList){
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view =super.getView(position, convertView, parent);
+
+                TextView textView=(TextView) view.findViewById(android.R.id.text1);
+
+                textView.setTextColor(Color.BLUE);
+
+                return view;
+            }
+        };
+
 
         listView.setAdapter(adapter);
 
